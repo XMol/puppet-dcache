89fef9cfe615c7a9796951f7721ee9631269003b
Xavier Mol <xavier.mol@kit.edu>
2015-08-19 13:18:23 +0200

    default path (/etc/dcache) set by config_file is fine

--- manifests/services/spacemanager.pp
+++ manifests/services/spacemanager.pp
@@ -1,7 +1,6 @@
 define dcache::services::spacemanager (
   $lga = $dcache::spacemanager_authz_link_group_file_name,
 ) {
-  dcache::config_file { basename($lga): 
-    path => "$lga",
+  dcache::config_file { basename($lga):
   }
 }
