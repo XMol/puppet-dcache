6cbe41c55058066b184dc1d44f885a3971d4f354
Xavier Mol <xavier.mol@kit.edu>
2015-08-19 10:54:38 +0200

    disable gpgchecks for dcache yum packages

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -5,11 +5,12 @@ class dcache::yum (
   if $version =~ /^(\d+)\.(\d+)\.\d+$/ {
     $minvers = "$1.$2"
     yumrepo { "dcache-${minvers} repo":
-      name => "dcache-${minvers}",
-      descr => "dCache ${minvers} rpms on GridKa mirror",
-      ensure => present,
-      enabled => 1,
-      baseurl => "ftp://mirror.gridka.de/www.dcache.org/${minvers}/";
+      name     => "dcache-${minvers}",
+      descr    => "dCache ${minvers} rpms on GridKa mirror",
+      ensure   => present,
+      enabled  => 1,
+      baseurl  => "ftp://mirror.gridka.de/www.dcache.org/${minvers}/",
+      gpgcheck => 0,
     }
   } else {
     fail("Failed to parse dCache's version; got '${version}'.")
