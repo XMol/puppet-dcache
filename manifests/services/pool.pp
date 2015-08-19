cadd2bba9f9c5fb9604a25df22f7bb2e137d206e
Xavier Mol <xavier.mol@kit.edu>
2015-08-19 13:17:31 +0200

    ensure_resources for $basedir

--- manifests/services/pool.pp
+++ manifests/services/pool.pp
@@ -2,10 +2,15 @@ define dcache::services::pool (
   $basedir    = $dcache::poolbasedir,
   $filesystem,
 ) {
+  
+  ensure_resource ('file', 'Symlink directory',
+    { ensure => directory, path => "${basedir}", }
+  )
+  
   file { "Symlink pool ${name}":
-    ensure => link,
-    path   => "${basedir}/${name}",
-    target => "${filesystem}/{$name}",
+    ensure  => link,
+    path    => "${basedir}/${name}",
+    target  => "${filesystem}/{$name}",
   }
   
   # Ensure GPFS mount.
