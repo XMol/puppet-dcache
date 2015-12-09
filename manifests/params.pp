f00be3915fe9534321ba9613a231e649f88e7776
Xavier Mol <xavier.mol@kit.edu>
2015-12-09 16:46:39 +0100

    Drop globals.pp - move all to params.pp. Introduce a hash for all dcache properties.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,116 +1,127 @@
 # Define default parameters for a dCache installation.
-class dcache::params inherits dcache::globals {
-  $debug           = false
-  $experiment      = $dcache::globals::experiment
-  $service_status  = 'stopped'
-  $manage_service  = true
-  $service_exec    = '/etc/init.d/dcache-server'
-  $manage_yum_repo = $dcache::globals::manage_yum_repo
+class dcache::params {
+  $debug = false
+  $experiment = $::experiment # Coming from Foreman host group.
+  case $experiment {
+    'urprod': {
+      $headnode = 'dcacheh1'
+      $hosts_service_chimera       = 'dcachepnfs'
+      $hosts_service_srm           = 'dcachesrm'
+      $hosts_service_httpd         = 'dcachesrm'
+      $hosts_service_billing       = 'dcacheh1'
+      $hosts_service_broker        = 'dcacheh1'
+      $hosts_database_chimera      = 'dcachepnfsdb'
+      $hosts_database_srm          = 'dcachesrmdb'
+      $hosts_database_spacemanager = 'dcachesrmdb'
+      $hosts_database_pinmanager   = 'dcachesrmdb'
+    }
+    default: {
+      $headnode                    = "dc${experiment}h1"
+      $hosts_service_chimera       = "dc${experiment}pnfs"
+      $hosts_service_srm           = "dc${experiment}srm"
+      $hosts_service_httpd         = "dc${experiment}gplazma"
+      $hosts_service_billing       = "dc${experiment}h1"
+      $hosts_service_broker        = "dc${experiment}h1"
+      $hosts_database_chimera      = "dc${experiment}pnfsdb"
+      $hosts_database_srm          = "dc${experiment}srmdb"
+      $hosts_database_spacemanager = "dc${experiment}srmdb"
+      $hosts_database_pinmanager   = "dc${experiment}srmdb"
+    }
+  }
   
-  $domains = {}
-  $plugins = []
+  $manage_yum_repo = true
   
-  $chimera_db_host      = $dcache::globals::hosts_database_chimera
-  $chimera_db_name      = 'chimera'
-  $chimera_db_user      = $dcache::globals::dcache_user
-  $srm_db_host          = $dcache::globals::hosts_database_srm
-  $srm_db_name          = 'dcache'
-  $srm_db_user          = $dcache::globals::dcache_user
-  $spacemanager_db_host = $dcache::globals::hosts_database_srm
-  $spacemanager_db_name = 'srm'
-  $spacemanager_db_user = $dcache::globals::dcache_user
-  $pinmanager_db_host   = $dcache::globals::hosts_database_pinmanager
-  $pinmanager_db_name   = 'srm'
-  $pinmanager_db_user   = $dcache::globals::dcache_user
+  $dcache_env = '/etc/dcache.env'
+  $dcache_paths_etc = '/etc/dcache'
+  $dcache_layout_dir = "$dcache_paths_etc/layouts"
+  $dcache_paths_admin = "$dcache_paths_etc/admin"
+  $dcache_paths_setup = "$dcache_paths_etc/dcache.conf"
+  $dcache_paths_config = '/var/lib/dcache/config'
+  $dcache_paths_gridsecurity = '/etc/grid-security'
+  $dcache_paths_plugins = '/usr/share/dcache/plugins'
+  $service_exec_orig = '/etc/rc.d/init.d/dcache-server'
+  $service_exec = '/etc/rc.d/init.d/dcache'
+  $dcache_user = 'dcache'
+  $dcache_uid = 500
+  $dcache_group = 'dcache'
+  $dcache_gid = 600
+  $poolbasedir = '/export'
+  $poolmanager_conf = "$dcache_paths_config/poolmanager.conf"
+  $admin_authorized_keys = "$dcache_paths_admin/authorized_keys2"
+  $gplazma_conf = "$dcache_paths_etc/gplazma.conf"
+  $authzdb_file = "$dcache_paths_gridsecurity/storage-authzdb"
+  $gridmap_file = "$dcache_paths_gridsecurity/grid-mapfile"
+  $kpwd_file = "$dcache_paths_etc/dcache.kpwd"
+  $vorolemap_file = "$dcache_paths_gridsecurity/grid-vorolemap"
+  $dcache_hostcert = "$dcache_paths_gridsecurity/dcache/hostcert.pem"
+  $dcache_hostkey = "$dcache_paths_gridsecurity/dcache/hostkey.pem"
+  $infoprovider_tapeinfo = "$dcache_paths_etc/tape-info.xml"
+  $infoprovider_xml = "$dcache_paths_etc/info-provider.xml"
+  $linkgroupauthconf = "$dcache_paths_etc/LinkGroupAuthorization.conf"
   
-  $billing_text_format_door_request_info_message = '$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$$$storage.storageClass$@$storage.hsm$$$$else$<Unknown>$endif$ $transactionTime$ $queuingTime$ ["$owner$":$uid$:$gid$:$client$] {$rc$:"$message$"}'
-  $billing_text_format_mover_info_message        = '$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$$$storage.storageClass$@$storage.hsm$$$$else$<Unknown>$endif$ $transferred$ $connectionTime$ $created$ {$protocol$} [$initiator$] [p2p=$p2p$] {$rc$:"$message$"}'
-  
-  $broker_host = 'localhost'
-  
-  $cleaner_enable_hsm = false
-  
-  $dcache_user                     = $dcache::globals::dcache_user
-  $dcache_group                    = $dcache::globals::dcache_group
-  $dcache_layout                   = "$::fqdn"
-  $dcache_dirs_layout              = "${dcache::globals::dcache_dirs_etc}/layouts"
-  $dcache_dirs_etc                 = "${dcache::globals::dcache_dirs_etc}"
-  $dcache_dirs_plugins             = '/usr/share/dcache/plugins'
-  $dcache_env                      = '/etc/dcache.env'
-  $dcache_setup                    = "${dcache::globals::dcache_dirs_etc}/dcache.conf"
-  $admin_paths_authorized_key      = "${dcache::globals::dcache_dirs_etc}/admin/authorized_keys2"
-  $dcache_authn_hostcert_key       = "${dcache::globals::dcache_dirs_grid_security}/dcache/hostkey.pem"
-  $dcache_authn_hostcert_cert      = "${dcache::globals::dcache_dirs_grid_security}/dcache/hostcert.pem"
-  $dcache_enable_overwrite         = false
-  $dcache_enable_space_reservation = false
-  $dcache_java_memory_heap         = undef
-
-  $dcap_authz_anonymous_operations = 'NONE'
-  $dcap_authz_readonly             = true
-  $dcap_mover_queue                = 'dcapq'
-  $dcap_limits_clients             = '-1'
-  
-  $ftp_performance_marker_period = 10
-  $ftp_mover_queue               = 'gridftpq'
-  $ftp_limits_clients            = '-1'
-  $ftp_limits_retries            = '3'
-  $ftp_authn_protocol            = 'gsi'
-  
-  $gplazma_configuration_file = "${dcache::globals::dcache_dirs_etc}/gplazma.conf"
-  $gplazma_gridmap_file       = "${dcache::globals::dcache_dirs_grid_security}/grid-mapfile"
-  $gplazma_authzdb_file       = "${dcache::globals::dcache_dirs_grid_security}/storage_authzdb"
-  $gplazma_vorolemap_file     = "${dcache::globals::dcache_dirs_grid_security}/grid-vorolemap"
-  $gplazma_kpwd_file          = "${dcache::globals::dcache_dirs_etc}/dcache.kpwd"
-  
-  $httpd_enable_authn                     = true
-  $httpd_authz_admin_gid                  = 600
-  $httpd_html_dcache_instance_name        = 'My dCache'
-  $httpd_html_dcache_instance_description = 'A dCache setup with default description.'
-  
-  $info_provider_site_unique_id                       = 'FZK_LCG2'
-  $info_provider_se_unique_id                         = 'UNKNOWNVALUE'
-  $info_provider_glue_se_status                       = 'Closed'
-  $info_provider_dcache_quality_level                 = 'development'
-  $info_provider_dcache_architecture                  = 'tape'
-  $info_provider_paths_tape_info                      = "${dcache::globals::dcache_dirs_etc}/tape_info.xml"
-  $info_provider_configuration_site_specific_location = "${dcache::globals::dcache_dirs_etc}/info-provider.xml"
-  $info_provider_http_host                            = $dcache::globals::hosts_service_httpd
-  
-  $nfs_mover_queue = 'nfsq'
-  $nfs_version     = '3,4.1'
-  
-  $pnfsmanager_default_retention_policy          = 'REPLICA'
-  $pnfsmanager_default_access_latency            = 'ONLINE'
-  $pnfsmanager_enable_full_path_permission_check = false
-
-  $poolmanager_setup_file = '/var/lib/dcache/config'
-  
-  $poolbasedir               = $dcache::globals::poolbasedir
-  $pool_path                 = "${dcache::globals::poolbasedir}/\${pool.name}"
-  $pool_wait_for_files       = '${pool.path}/data'
-  $pool_queues               = 'dcapq,gridftpq,httpq,nfsq,xrootdq'
-  $pool_plugins_meta         = 'org.dcache.pool.repository.meta.db.BerkeleyDBMetaDataRepository'
-  $pool_mover_xrootd_plugins = undef
-  
-  $spacemanager_enable_unreserved_uploads_to_linkgroups = false
-  $spacemanager_authz_link_group_file_name              = "${dcache::globals::dcache_dirs_etc}/LinkGroupAuthorization.conf"
-  
-  $srm_enable_pin_online_files = false
-  $srm_net_host                = $dcache::globals::hosts_service_srm
-  
-  $transfermanagers_mover_queue = 'gridftpq'
-  
-  $webdav_authz_allowed_paths        = ['/pnfs/gridka.de/ops', '/pnfs/gridka.de/dteam']
-  $webdav_authz_anonymous_operations = 'READONLY'
-  $webdav_authn_protocol             = 'https-jglobus'
-  $webdav_authn_require_client_cert  = true
-  $webdav_mover_queue                = 'httpq'
-  
-  $xrootd_mover_queue = 'xrootdq'
-  $xrootd_plugins     = undef
-  
-  case "${::osfamily}:${::operatingsystemmajrelease}" {
-    'RedHat:6': { $java_package = 'java-1.8.0-openjdk-1.8.0.51' }
-    default:    { $java_package = 'jdk1.8.0_45' }
+  # Configuration parameters going into dcache.conf.
+  $setup = {
+    'admin.paths.authorized-keys' => "$admin_authorized_keys",
+    'billing.text.format.door-request-info-message' => '$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$$$storage.storageClass$@$storage.hsm$$$$else$<Unknown>$endif$ $transactionTime$ $queuingTime$ ["$owner$":$uid$:$gid$:$client$] {$rc$:"$message$"}',
+    'billing.text.format.mover-info-message' => '$date$ [$cellType$:$cellName$:$type$] [$pnfsid$,$filesize$] [$path$] $if(storage)$$$$storage.storageClass$@$storage.hsm$$$$else$<Unknown>$endif$ $transferred$ $connectionTime$ $created$ {$protocol$} [$initiator$] [p2p=$p2p$] {$rc$:"$message$"}',
+    'chimera.db.host' => "$hosts_database_chimera",
+    'cleaner.enable.hsm' => 'false',
+    'dcache.authn.hostcert.key' => "$dcache_hostkey",
+    'dcache.authn.hostcert.cert' => "$dcache_hostcert",
+    'dcache.broker.host' => "$hosts_service_broker",
+    'dcache.enable.overwrite' => 'false',
+    'dcache.enable.space-reservation' => 'false',
+    'dcache.user' => "$dcache_user",
+    'dcache.group' => "$dcache_group",
+    'dcap.authz.anonymous-operations' => 'READONLY',
+    'dcap.authz.readonly' => 'true',
+    'dcap.limits.clients' => '"-1"',
+    'dcap.mover.queue' => 'dcapq',
+    'ftp.authn.protocol' => 'gsi',
+    'ftp.limits.retries' => '3',
+    'ftp.limits.clients' => '"-1"',
+    'ftp.mover.queue' => 'gridftpq',
+    'ftp.performance-marker-period' => '10',
+    'ftp.root' => '/',
+    'gplazma.configuration.file' => "$gplazma_conf",
+    'gplazma.authzdb.file' => "$authzdb_file",
+    'gplazma.gridmap.file' => "$gridmap_file",
+    'gplazma.kpwd.file' => "$kpwd_file",
+    'gplazma.vorolemap.file' => "$vorolemap_file",
+    'httpd.enable.authn' => 'true',
+    'httpd.authz.admin-gid' => "$dcache_gid",
+    'httpd.html.dcache-instance-name' => '${info-provider.se-unique-id}',
+    'httpd.html.dcache-instance-description' => '${info-provider.se-unique-id}',
+    'info-provider.site-unique-id' => 'FZK-LCG2',
+    'info-provider.se-unique-id' => 'NEEDS CHANGE!',
+    'info-provider.glue-se-status' => 'Closed',
+    'info-provider.dcache-quality-level' => 'development',
+    'info-provider.dcache-architecture' => 'disk',
+    'info-provider.paths.tape-info' => "$infoprovider_tapeinfo",
+    'info-provider.http.host' => "$hosts_service_httpd",
+    'info-provider.configuration.site-specific.location' => "$infoprovider_xml",
+    'nfs.mover.queue' => 'nfsq',
+    'pinmanager.db.host' => "$hosts_database_pinmanager",
+    'pinmanager.db.name' => 'dcache',
+    'pnfsmanager.default-retention-policy' => 'REPLICA',
+    'pnfsmanager.default-access-latency' => 'ONLINE',
+    'poolmanager.setup.file' => "$poolmanager_conf",
+    'pool.path' => "$poolbasedir/\${pool.name}",
+    'pool.wait-for-files' => '${pool.path}/data',
+    'pool.plugins.meta' => 'org.dcache.pool.repository.meta.db.BerkeleyDBMetaDataRepository',
+    'pool.queues' => '${dcap.mover.queue},${ftp.mover.queue},${xrootd.mover.queue},${webdav.mover.queue}',
+    'spacemanager.authz.link-group-file-name' => "$linkgroupauthconf",
+    'spacemanager.db.host' => "$hosts_database_spacemanager",
+    'spacemanager.db.name' => 'dcache',
+    'srm.db.host' => "$hosts_database_srm",
+    'srm.db.name' => 'dcache',
+    'srm.enable.pin-online-files' => 'false',
+    'transfermanagers.mover.queue' => '${ftp.mover.queue}',
+    'webdav.authz.allowed-paths' => '/pnfs/gridka.de/ops:/pnfs/gridka.de/dteam',
+    'webdav.authz.anonymous-operations' => 'READONLY',
+    'webdav.authn.protocol' => 'https-jglobus',
+    'webdav.authn.require-client-cert' => 'true',
+    'webdav.mover.queue' => 'httpq',
+    'xrootd.mover.queue' => 'xrootdq',
   }
 }
