b6f5ea08e40d2f517865f95bd5476150306ab3d9
Xavier Mol <xavier.mol@kit.edu>
2015-12-10 10:05:01 +0100

    Don't monitor the service status with Puppet. It cannot be done with ambiguities.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,22 +1,26 @@
 class dcache::service (
-  $status       = $dcache::service_status,
-  $service      = $dcache::manage_service,
   $service_exec = $dcache::service_exec,
+  $service_exec_orig = $dcache::service_exec_orig,
 ) {
   
-  if $service == true {
-    exec { "$service_exec":
-      refreshonly => true,
-      command     => "ln -fs $(rpm -ql dcache-${dcache::dcache_version} | sed -n '\\+/dcache$+ p) ${service_exec}",
-      path        => '/bin',
-      subscribe   => Package['dcache'],
-    }
-    
-    service { 'dcache':
-      ensure  => $status,
-      enable  => $service,
-      require => Exec["${service_exec}"],
-    }
+  exec { "Create symbolic link $service_exec":
+    refreshonly => true,
+    command     => "ln -fs $(rpm -ql dcache-${dcache::dcache_version} | grep -e '/dcache$') ${service_exec}",
+    path        => '/bin',
+    subscribe   => Package['dcache'],
+  }
+  
+  exec { "Remove original dCache init script":
+    command => "/bin/rm $service_exec_orig",
+    onlyif => "test -L $service_exec_orig",
+    subscribe   => Package['dcache'],
+  }
+  
+  exec { "Add dcache to check-config":
+    refreshonly => true,
+    command => "chkconfig --add $(basename $service_exec)",
+    path => '/bin',
+    subscribe   => Package['dcache'],
   }
   
 }
