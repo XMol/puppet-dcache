dc4c2c80b0c841cfcf7aadd9a1c11b71944fb6b0
Xavier Mol <xavier.mol@kit.edu>
2015-12-10 09:41:31 +0100

    Update dcache::config to only create the dcache::domain resources for this host.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,15 +1,7 @@
-# Apply config changes for dCache.
-# Right now, because versatile modules aren ot ready, this means
-# sourcing the layout file and all configuration files related to the
-# services from a Git repository.
 class dcache::config (
   $domains = $dcache::domains,
 ) {
-  
-  dcache::layout { 'Get layout': }
-
-  dcache::config_file { 'dcache.conf': }
-  
-  create_resources('dcache::domain', $domains)
+  # Create only the domains for this host.
+  create_resources('dcache::domain', $domains[$::hostname])
   
 }
