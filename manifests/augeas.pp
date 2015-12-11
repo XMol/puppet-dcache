7ea34879d1826a1c07661d5f2a9730e567c4347b
Xavier Mol <xavier.mol@kit.edu>
2015-12-11 16:21:09 +0100

    Add augeas.pp for sourcing the dCache specific Augeas lenses.

---
+++ manifests/augeas.pp
@@ -0,0 +1,15 @@
+# Put the dCache specific Augeas lenses from this Puppet module in
+# the default Augeas search path.
+class dcache::augeas (
+  $augeas_include_dir = '/usr/share/augeas/lenses',
+) {
+  $dcache_aug_lenses = [
+    'dcachelayout.aug', 'gplazma.aug', 'gridmapfile.aug', 'kpwd.aug',
+    'linkgroupauthorization.aug', 'poolmanager.aug', 'storageauthzdb.aug',
+  ]
+  file { $dcache_aug_lenses:
+    path => "$augeas_include_dir/$title",
+    source => "puppet:///modules/$module_name/$title",
+    require => Package['augeas'],
+  }
+}
