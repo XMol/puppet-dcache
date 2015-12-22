354585ffcf0948b29c1b1d4b9e740cc95d367671
Xavier Mol <xavier.mol@kit.edu>
2015-12-22 14:00:46 +0100

    Comment on the 'properties' parameter.

--- manifests/domain.pp
+++ manifests/domain.pp
@@ -8,6 +8,10 @@ define dcache::domain (
   each($services) |$service, $shash| {
     # $shash may be unset, but is required to be a hash, even an empty one
     # does suffice.
+    # Every resource among the dcache::services must at least accept a
+    # parameter 'properties', simply because that parameter is evaluated
+    # by the dcache::layout class. For the actual resource, that parameter
+    # is meaningless.
     if $shash {
       ensure_resource("dcache::services::$service", $service, $shash)
     } else {
