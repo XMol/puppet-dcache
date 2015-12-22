f43dc89ee7d6c6ad514829f8ab916a6b66711eb1
Xavier Mol <xavier.mol@kit.edu>
2015-12-22 13:55:28 +0100

    Commit final version of gplazma Puppet resource type definition, supporting all of GridKa-typical authentication methods.

--- manifests/services/gplazma.pp
+++ manifests/services/gplazma.pp
@@ -1,15 +1,31 @@
 define dcache::gplazma (
-  $gplazma       = $dcache::gplazma_configuration_file,
-  $kpwd          = $dcache::gplazma_kpwd_file,
-  $grid_security = $dcache::globals::dcache_dirs_grid_security,
-  $gridmap       = $dcache::gplazma_gridmap_file,
-  $authzdb       = $dcache::gplazma_authzdb_file,
-  $vorolemap     = $dcache::gplazma_vorolemap_file,
+  $properties = {},
+  $gplazma = {},
+  $authzdb = {},
+  $gridmap = {},
+  $kpwd = {},
+  $vorolemap = {},
 ) {
-  dcache::config_file { [$gplazma, $kpwd]: }
-  dcache::config_file { [$gridmap, $authzdb, $vorolemap]:
-    path => "$grid_security",
+  
+  if $gplazma {
+    ensure_resource('dcache::gplazma::gplazma_conf', $gplazma)
+  }
+  
+  if $authzdb {
+    ensure_resource('dcache::gplazma::authzdb', $authzdb)
+  }
+  
+  if $gridmap {
+    ensure_resource('dcache::gplazma::gridmap', $gridmap)
   }
 
-  if $dcache::debug { notify { "realized $title":  } }
+  # Reuse the same resource for the vorolemap.
+  if $vorolemap {
+    ensure_resource('dcache::gplazma::gridmap', $vorolemap)
+  }
+  
+  if $kpwd {
+    ensure_resource('dcache::gplazma::kpwd', $kpwd)
+  }
+
 }
