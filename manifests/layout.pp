5ba7887755b3927662dcb1ae920e573db9431206
Xavier Mol <xavier.mol@kit.edu>
2016-01-12 17:02:16 +0100

    Adapt layout manifest to improved dcachelayout Augeas lens.

--- manifests/layout.pp
+++ manifests/layout.pp
@@ -7,30 +7,73 @@ define dcache::layout (
 ) {
   require dcache::augeas
   
-  Augeas { incl => "$layout_file", }
+  Augeas {
+    lens => 'DCacheLayout.lns',
+    incl => "$layout_file",
+  }
   
-  each($layout['properties']) |$prop_key, $prop_value| {
-    augeas { "Add bare property '$prop_key' to the layout file":
-      changes => "set ./$prop_key \"$prop_value\"",
+  if has_key($layout, 'properties') {
+    validate_hash($layout['properties'])
+    augeas { "Add bare properties to '$layout_file'":
+      changes => flatten([
+        "defnode this properties",
+        map($layout['properties']) |$k, $v| { "set \$this/$k \"$v\"" },
+      ]),
     }
   }
   
-  each($layout['domains']) |$domain, $dhash| {
-    augeas { "Add domain '$domain' to the layout file":
-      changes => "set ./\\[domain\\][. = \"$domain\"] \"$domain\"",
-    }
-    each($dhash['properties']) |$prop_key, $prop_value| {
-      augeas { "Add $domain property $prop_key to the layout file":
-        changes => "set ./\\[domain\\]/$prop_key \"$prop_value\"",
+  if has_key($layout, 'domains') {
+    validate_hash($layout['domains'])
+    each($layout['domains']) |$domain, $dhash| {
+      validate_hash($dhash)
+      augeas { "Add domain '$domain' to '$layout_file'":
+        name => "augeas_create_$domain",
+        changes => "set domain[. = \"$domain\"] \"$domain\"",
       }
-    }
-    each($dhash['services']) |$service, $shash| {
-      augeas { "Add service '$service' to domain '$domain' in the layout file":
-        changes => "set ./\\[domain\\]/\\[service\\][. = \"$domain/$service\"] \"$domain/$service\"",
+      
+      if has_key($dhash, 'properties') {
+        augeas { "Add properties to domain '$domain' in '$layout_file'":
+          require => Augeas["augeas_create_$domain"],
+          changes => flatten([
+            "defnode this domain[. = \"$domain\"]/properties",
+            map($dhash['properties']) |$k, $v| { "set \$this/$k \"$v\"" }
+          ]),
+        }
       }
-      each($shash['properties']) |$prop_key, $prop_value| {
-        augeas { "Add $service property $prop_key to the layout file":
-          changes => "set ./\\[domain\\]/\\[service\\]/$prop_key \"$prop_value\"",
+      
+      if has_key($dhash, 'services') {
+        # The list of services cannot be a hash, because that would disallow
+        # having more than one instance of the same service in one domain
+        # (which would be rare, but nevertheless is supported by dCache).
+        validate_array($dhash['services'])
+        each($dhash['services']) |$service| {
+          # $service now either is a string or a hash. In the latter case,
+          # some more properties are supplied, with need to be written to
+          # the layout file.
+          if is_hash($service) {
+            # We're iterating over exactly one element here just because we
+            # need to store the key.
+            each($service) |$sk, $sv| {
+              validate_hash($sv)
+              augeas { "Add '$domain/$sk' to '$layout_file'":
+                require => Augeas["augeas_create_$domain"],
+                changes => flatten([
+                  "defnode this domain[. = \"$domain\" and service = \"$domain/$sk\"]/service",
+                  "set \$this \"$domain/$sk\"",
+                  "defnode this \$this/properties",
+                  map($sk) |$k, $v| { "set \$this/$k \"$v\"" },
+                ]),
+              }
+            }
+          } else {
+            # $service is a bare string, no further properties to add to the
+            # layout file.
+            augeas { "Add '$domain/$service' to '$layout_file'":
+              require => Augeas["augeas_create_$domain"],
+              changes =>
+                "set domain[. = \"$domain\" and service = \"$domain/$service\"]/service \"$domain/$service\"",
+            }
+          }
         }
       }
     }
