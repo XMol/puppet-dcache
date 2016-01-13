c4e32a440246a00900bc6a04970454f90492b8ee
Xavier Mol <xavier.mol@kit.edu>
2016-01-13 09:14:13 +0100

    Generate the dcache.conf file with Augeas.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,17 @@
 class dcache::config (
+  $setup = $dcache::setup,
   $layout = $dcache::layout,
 ) {
   # Initiate the domains for this host.
   create_resources('dcache::domain', $layout['domains'])
   
   include dcache::augeas
+  
+  augeas { "Generate the dcache.conf file ('$setup')":
+    incl => "$setup",
+    lens => 'Simplevars',
+    changes => map($setup) |$k, $v| { "set \"$k\" \"$v\"" },
+  }
   
   dcache::layout { "Generate the dCache layout file":
     layout => $layout,
