2609e9349e13963f43674aa525f5ba2b5fa01d28
Xavier Mol <xavier.mol@kit.edu>
2016-02-23 11:09:16 +0100

    Updating for Puppet-Lint one more time.

--- manifests/dcfiles/poolmanager/pm_units.pp
+++ manifests/dcfiles/poolmanager/pm_units.pp
@@ -8,40 +8,40 @@ define dcache::dcfiles::poolmanager::pm_units (
   
   validate_array($net)
   each($net) |$unit| {
-    augeas { "Create net-unit '$unit' in '$setup'":
+    augeas { "Create net-unit '${unit}' in '${setup}'":
       changes => [
-        "defnode this psu_create_unit[. = '$unit' and type = 'net'] '$unit'",
-        "set \$this/type 'net'",
+        "defnode this psu_create_unit[. = '${unit}' and type = 'net'] '${unit}'",
+        'set \$this/type "net"',
       ]
     }
   }
   
   validate_array($store)
   each($store) |$unit| {
-    augeas { "Create store-unit '$unit' in '$setup'":
+    augeas { "Create store-unit '${unit}' in '${setup}'":
       changes => [
-        "defnode this psu_create_unit[. = '$unit' and type = 'store'] '$unit'",
-        "set \$this/type 'store'",
+        "defnode this psu_create_unit[. = '${unit}' and type = 'store'] '${unit}'",
+        'set \$this/type "store"',
       ]
     }
   }
   
   validate_array($protocol)
   each($protocol) |$unit| {
-    augeas { "Create protocol-unit '$unit' in '$setup'":
+    augeas { "Create protocol-unit '${unit}' in '${setup}'":
       changes => [
-        "defnode this psu_create_unit[. = '$unit' and type = 'protocol'] '$unit'",
-        "set \$this/type 'protocol'",
+        "defnode this psu_create_unit[. = '${unit}' and type = 'protocol'] '${unit}'",
+        'set \$this/type "protocol"',
       ]
     }
   }
   
   validate_array($dcache)
   each($dcache) |$unit| {
-    augeas { "Create dcache-unit '$unit' in '$setup'":
+    augeas { "Create dcache-unit '${unit}' in '${setup}'":
       changes => [
-        "defnode this psu_create_unit[. = '$unit' and type = 'dcache'] '$unit'",
-        "set \$this/type 'dcache'",
+        "defnode this psu_create_unit[. = '${unit}' and type = 'dcache'] '${unit}'",
+        'set \$this/type "dcache"',
       ]
     }
   }
