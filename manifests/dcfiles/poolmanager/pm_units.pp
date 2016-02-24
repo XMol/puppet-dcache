35575182fb4af630bc3003608b4900b5957f45c9
Xavier Mol <xavier.mol@kit.edu>
2016-02-24 08:48:27 +0100

    Ensure that PoolManager units and pool constructs are not declared more often than exactly once.

--- manifests/dcfiles/poolmanager/pm_units.pp
+++ manifests/dcfiles/poolmanager/pm_units.pp
@@ -8,41 +8,49 @@ define dcache::dcfiles::poolmanager::pm_units (
   
   validate_array($net)
   each($net) |$unit| {
-    augeas { "Create net-unit '${unit}' in '${setup}'":
-      changes => [
-        "defnode this psu_create_unit[. = '${unit}' and type = 'net'] '${unit}'",
-        'set \$this/type "net"',
-      ]
+    if !defined(Augeas["Create net-unit '${unit}' in '${setup}'"]) {
+      augeas { "Create net-unit '${unit}' in '${setup}'":
+        changes => [
+          "defnode this psu_create_unit[. = '${unit}' and type = 'net'] '${unit}'",
+          'set \$this/type "net"',
+        ]
+      }
     }
   }
   
   validate_array($store)
   each($store) |$unit| {
-    augeas { "Create store-unit '${unit}' in '${setup}'":
-      changes => [
-        "defnode this psu_create_unit[. = '${unit}' and type = 'store'] '${unit}'",
-        'set \$this/type "store"',
-      ]
+    if !defined(Augeas["Create store-unit '${unit}' in '${setup}'"]) {
+      augeas { "Create store-unit '${unit}' in '${setup}'":
+        changes => [
+          "defnode this psu_create_unit[. = '${unit}' and type = 'store'] '${unit}'",
+          'set \$this/type "store"',
+        ]
+      }
     }
   }
   
   validate_array($protocol)
   each($protocol) |$unit| {
-    augeas { "Create protocol-unit '${unit}' in '${setup}'":
-      changes => [
-        "defnode this psu_create_unit[. = '${unit}' and type = 'protocol'] '${unit}'",
-        'set \$this/type "protocol"',
-      ]
+    if !defined(Augeas["Create protocol-unit '${unit}' in '${setup}'"]) {
+      augeas { "Create protocol-unit '${unit}' in '${setup}'":
+        changes => [
+          "defnode this psu_create_unit[. = '${unit}' and type = 'protocol'] '${unit}'",
+          'set \$this/type "protocol"',
+        ]
+      }
     }
   }
   
   validate_array($dcache)
   each($dcache) |$unit| {
-    augeas { "Create dcache-unit '${unit}' in '${setup}'":
-      changes => [
-        "defnode this psu_create_unit[. = '${unit}' and type = 'dcache'] '${unit}'",
-        'set \$this/type "dcache"',
-      ]
+    if !defined(Augeas["Create dcache-unit '${unit}' in '${setup}'"]) {
+      augeas { "Create dcache-unit '${unit}' in '${setup}'":
+        changes => [
+          "defnode this psu_create_unit[. = '${unit}' and type = 'dcache'] '${unit}'",
+          'set \$this/type "dcache"',
+        ]
+      }
     }
   }
 }
