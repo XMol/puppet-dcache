e1ec1e0d0cd563c097c44ea2006011b8a8d809d8
Xavier Mol <xavier.mol@kit.edu>
2016-02-25 08:57:21 +0100

    Sort the generated configuration file after an update.

--- manifests/dcfiles/simple.pp
+++ manifests/dcfiles/simple.pp
@@ -7,5 +7,13 @@ define dcache::dcfiles::simple (
     incl    => $file,
     lens    => 'Simplevars.lns',
     changes => map($augeas) |$k, $v| { "set '${k}' '${v}'" },
+  } ~>
+  # Since $file is a simple list of arbitrary key value pairs, it is more
+  # readable to have them sorted. Specifically for dcache.conf, where
+  # properties for the same service are prefixed the same.
+  exec { "Sort '${file}'":
+    refreshonly => true,
+    path        => '/bin',
+    command     => "sort -o ${file} ${file}",
   }
 }
