009ea59a7b58de8e4d8a4dfdf2879211786a30c8
Xavier Mol <xavier.mol@kit.edu>
2016-03-11 10:25:18 +0100

    Add linkgroupauth dcfile resource.

---
+++ manifests/dcfiles/linkgroupauth.pp
@@ -0,0 +1,22 @@
+define dcache::dcfiles::linkgroupauth (
+  $file,
+  $augeas,
+) {
+  
+  Augeas {
+    lens => 'LinkGroupAuthorization.lns', # Coming with this module.
+    incl => $file,
+  }
+  
+  each($augeas) |$lgroup, $entries| {
+    validate_array($entries)
+    augeas { "Manage '${lgroup}' in '${file}'": 
+      changes => flatten(
+        "defnode this LinkGroup[. = '${lgroup}'] '${lgroup}'",
+        map($entries) |$entry| {
+          "set \$this/entry[. = '${entry}'] '${entry}'"
+        },
+      ),
+    }
+  }
+}
