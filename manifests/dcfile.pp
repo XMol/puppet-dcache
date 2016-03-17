7aae129546378f1da8be408f00aa8e02b4da2421
Xavier Mol <xavier.mol@kit.edu>
2016-03-17 11:34:30 +0100

    Introduce template variants for file management and order them for priority.

--- manifests/dcfile.pp
+++ manifests/dcfile.pp
@@ -1,29 +1,29 @@
 define dcache::dcfile (
   $file,
-  $content,
   $source,
+  $content,
+  $template = {},
   $augeas = {},
-  $resource,
+  $resource = '', # Only required for $template and $augeas.
 ) {
+  validate_hash($template)
   validate_hash($augeas)
-  if bool2num(empty($content)) + bool2num(empty($source)) + bool2num(empty($augeas)) < 2 {
-    fail('Only one out of $content, $source or $augeas ought to be set!')
-  }
-  
   File {
     path  => $file,
     owner => $dcache::user,
     group => $dcache::group,
   }
   
-  if !empty($content) {
-    file { "Set content of '${file}'":
-      content => $content,
-    }
-  } elsif !empty($source) {
+  if !empty($source) {
     file { "Source content of '${file}'":
       source => $source,
     }
+  } elsif !empty($content) {
+    file { "Set content of '${file}'":
+      content => $content,
+    }
+  } elsif !empty($template) {
+    epp("dcache/${resource}.epp", { 'template' => $template })
   } elsif !empty($augeas) {
     create_resources("dcache::dcfiles::${resource}", { $title => { file => $file, augeas => $augeas, } })
   }
--- manifests/init.pp
+++ manifests/init.pp
@@ -11,68 +11,78 @@ class dcache (
   # There is the option to manage this file by giving its content explicitly
   # or a uri where the file can be sourced from or by using Augeas.
   $env = $dcache::params::env,
-  $env_content = '',
   $env_source = '',
+  $env_content = '',
+  $env_template = {},
   $env_augeas = {},
   
   # dCache's central dcache.conf file.
   $setup = $dcache::params::setup,
-  $setup_content = '',
   $setup_source = '',
+  $setup_content = '',
+  $setup_template = {},
   $setup_augeas = {},
   
   $layout = $dcache::params::layout,
-  $layout_content = '',
   $layout_source = '',
+  $layout_content = '',
+  $layout_template = {},
   $layout_augeas = {},
   
   $poolmanager = $dcache::params::poolmanager,
-  $poolmanager_content = '',
   $poolmanager_source = '',
+  $poolmanager_content = '',
+  $poolmanager_template = {},
   $poolmanager_augeas = {},
   
   $authorized_keys = $dcache::params::authorized_keys,
-  $authorized_keys_content = '',
   $authorized_keys_source = '',
+  $authorized_keys_content = '',
+  $authorized_keys_template = {},
   $authorized_keys_augeas = {},
   
   $gplazma = $dcache::params::gplazma,
-  $gplazma_content = '',
   $gplazma_source = '',
-  # The gplazma.conf file cannot be managed properly with Augeas, since it
-  # may contain duplicate lines and the order of the file's content matters.
+  $gplazma_content = '',
+  # The gplazma.conf is tricky to be managed by Augeas or template and
+  # defining it in YAML style doesn't save effort anyway..
   
   $authzdb = $dcache::params::authzdb,
-  $authzdb_content = '',
   $authzdb_source = '',
+  $authzdb_content = '',
+  $authzdb_template = {},
   $authzdb_augeas = {},
   
   $gridmap = $dcache::params::gridmap,
-  $gridmap_content = '',
   $gridmap_source = '',
+  $gridmap_content = '',
+  $gridmap_template = {},
   $gridmap_augeas = {},
   
   $kpwd = $dcache::params::kpwd,
-  $kpwd_content = '',
   $kpwd_source = '',
+  $kpwd_content = '',
+  $kpwd_template = {},
   $kpwd_augeas = {},
   
   $vorolemap = $dcache::params::vorolemap,
-  $vorolemap_content = '',
   $vorolemap_source = '',
+  $vorolemap_content = '',
+  $vorolemap_template = {},
   $vorolemap_augeas = {},
   
   $tapeinfo = $dcache::params::tapeinfo,
-  $tapeinfo_content = '',
   $tapeinfo_source = '',
+  $tapeinfo_content = '',
   
   $infoprovider = $dcache::params::infoprovider,
-  $infoprovider_content = '',
   $infoprovider_source = '',
+  $infoprovider_content = '',
   
   $linkgroupauth = $dcache::params::linkgroupauth,
-  $linkgroupauth_content = '',
   $linkgroupauth_source = '',
+  $linkgroupauth_content = '',
+  $linkgroupauth_template = {},
   $linkgroupauth_augeas = {},
 
 ) inherits dcache::params {
