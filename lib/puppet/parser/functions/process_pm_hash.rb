dabbe25faa497baa026b07ede0170a3da2c6528f
Xavier Mol <xavier.mol@kit.edu>
2016-04-06 15:16:10 +0200

    Break the scope gates.

--- lib/puppet/parser/functions/process_pm_hash.rb
+++ lib/puppet/parser/functions/process_pm_hash.rb
@@ -1,121 +1,138 @@
 module Puppet::Parser::Functions
   newfunction(:process_pm_hash, :type => :rvalue) do |args|
     units = {
-      :store    => [],
-      :net      => [],
-      :protocol => [],
-      :dcache   => [],
+      'store'    => Array.new,
+      'net'      => Array.new,
+      'protocol' => Array.new,
+      'dcache'   => Array.new,
     }
     ugroups = Hash.new({})
-    pools = Array.new()
+    pools = Array.new
     pgroups = Hash.new({})
     links = Hash.new({})
     lgroups = Hash.new({})
-    lgroups_allowances = {
-      :online    => false,
-      :nearline  => false,
-      :replica   => false,
-      :custodial => false,
-      :output    => false,
-    }
-    
-    # Override the assignment method for the individual hashes so we can
-    # implement "recursive" parsing.
-    
+
+    # Update the collection of units by adding them to each
+    # unit class. Make also sure that the list of units
+    # doesn't contain duplicates.
+    # We have to define this method in this ugly way, because otherwise
+    # units is not in the scope of the method (Google "Ruby scope gates").
+    Kernel.send(:define_method, :new_units) do |uhash|
+      uhash.each do |key, val|
+        units[key] += val
+        units[key].uniq!
+      end
+    end
+
     # For each new unit group add all units to the respective list of its
     # kind. Keep a map of ugroup name to all its units in the ugroups hash.
-    def ugroups.[]= (key, val)
-      super(key, val.values.flatten)
-      val.each do |uclass, ulist|
-        units[uclass] += ulist
-      end
+    Kernel.send(:define_method, :new_ugroup) do |ugroup, uhash|
+      puts "Assign ugroups[#{ugroup}] = #{uhash.values.flatten}"
+      ugroups[ugroup] = uhash.values.flatten
+      new_units(uhash)
     end
-    
+
     # For each new pool group, add all pools to the list of all pools.
-    def pgroups.[]= (key, val)
-      super(key, val)
-      val.each do |pgroup, my_pools|
-        pools += my_pools
-      end
+    Kernel.send(:define_method, :new_pgroup) do |pgroup, plist|
+      puts "Assign pgroups[#{pgroup}] = #{plist}"
+      pgroups[pgroup] = plist
+      pools += plist
+      pools.uniq!
     end
-    
+
     # links are broken down into three subparts:
     #   - ugroups
     #     Add every ugroup from this link to the hash of all ugroups. Keep
-    #     a mapping of ugroup to all units in tmp.
+    #     a list of all ugroups (w/o their respective units).
     #   - pgroups
-    #     Update the hash of all pgroups and the list of pools. Keep a
-    #     mapping of pgroup to pools in tmp.
+    #     Update the hash of all pgroups. Keep a list of pgroups.
     #   - prefs
     #     Only the link itself needs to now whether preferences were set.
-    def links.[]= (key, val)
-      tmp = Hash.new
-      
-      if val.key?(:ugroups)
-        val[:ugroups].each do |ugroup, uhash|
-          tmp[:ugroups][ugroup] = uhash.values.flatten
-          ugroups[ugroup] = uhash
-        end
-      else
-        tmp[:ugroups] = {}
-      end
-      
-      if val.key?(:pgroups)
-        val[:pgroups].each do |pgroup, my_pools|
-          tmp[:pgroups][pgroup] = my_pools
-          pgroups[pgroup] = my_pools
-        end
-      else
-        tmp[:groups] = {}
-      end
-      
-      tmp[:prefs] = val.key?(:prefs) ? val[:prefs] : {}
-      
-      super(key, tmp)
-      
-    end
+    Kernel.send(:define_method, :new_link) do |link, lhash|
+      my_link = {
+        :ugroups => [],
+        :pgroups => [],
+        :prefs   => {
+          'read'  => 0,
+          'write' => 0,
+          'cache' => 0,
+          'p2p'   => 0,
+        },
+      }
     
-    # lgroups
-    def lgroups.[]= (key, val)
-      if val.key?(:allowances)
-        lgroups[key][:allowance] = lgroups_allowances.merge(val[:allowances])
-      else
-        lgroups[key][:allowance] = lgroups_allowances
+      if lhash.key?('ugroups')
+        lhash['ugroups'].each do |ugroup, uhash|
+          new_ugroup(ugroup, uhash)
+          my_link[:ugroups] << ugroup
+        end
+        my_link[:ugroups].uniq!
+      end
+    
+      if lhash.key?('pgroups')
+        lhash['pgroups'].each do |pgroup, plist|
+          new_pgroup(pgroup, plist)
+          my_link[:pgroups] << pgroup
+        end
+        my_link[:pgroups].uniq!
       end
       
-      lgroups[key][:links] = Array.new()
-      if val.key?(:links)
-        val[:links].each do |link, link_details|
-          lgroups[key][:links] += link
-          links[link] = link_details
-        end
-      end
+      my_link[:prefs].merge!(lhash.fetch('prefs', {}))
+      
+      puts "Assign links[#{link}] = #{my_link}"
+      links[link] = my_link
     end
-    
-    args[0].each do |pm_class, members|
+
+    Kernel.send(:define_method, :new_lgroup) do |lgroup, lghash|
+      my_lgroup = {
+        :allowances => {
+          'online'    => false,
+          'nearline'  => false,
+          'replica'   => false,
+          'custodial' => false,
+          'output'    => false,
+        },
+        :links => Array.new(),
+      }
+      
+      my_lgroup[:allowances].merge!(lghash.fetch('allowances', {}))
+      
+      if lghash.key?('links')
+        lghash['links'].each do |link, lhash|
+          new_link(link, lhash)
+          my_lgroup[:links] << link
+        end
+        my_lgroup[:links].uniq!
+      end
+      
+      puts "Assign lgroups[#{lgroup}] = #{my_lgroup}"
+      lgroups[lgroup] = my_lgroup
+    end
+
+    args.each do |pm_class, members|
       case pm_class
-      when :units
-        units.merge!(members)
-      when :ugroups
-        ugroups.merge!(members)
-      when :pools
+      when 'units'
+        new_units(members)
+      when 'ugroups'
+        members.each do |ugroup, uhash|
+          new_ugroup(ugroup, uhash)
+        end
+      when 'pools'
         pools += members
-      when :pgroups
-        pgroups.merge!(members)
-      when :links
-        links.merge!(members)
-      when :lgroups
-        lgroups.merge!(members)
+        pools.uniq!
+      when 'pgroups'
+        members.each do |pgroup, plist|
+          new_pgroup(pgroup, plist)
+        end
+      when 'links'
+        members.each do |link, lhash|
+          new_link(link, lhash)
+        end
+      when 'lgroups'
+        members.each do |lgroup, lghash|
+          new_lgroup(lgroup, lghash)
+        end
       end
     end
-    
-    # Purge dublicates.
-    units.each do |uclass, ulist| ulist.uniq! end
-    ugroups.each do |ugroup, my_units| my_units.uniq! end
-    pools.uniq!
-    pgroups.each do |pgroup, my_pools| my_pools.uniq! end
-    links.each do |link| link[:pgroups].uniq! end
-    lgroups.each do |lgroup| lgroup[:links].uniq! end
     
     return {
       :units   => units,
