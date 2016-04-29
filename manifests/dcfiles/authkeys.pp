bc47679de193dc307fa29673c413a7dbbc4642d4
Xavier Mol <xavier.mol@kit.edu>
2016-04-29 15:10:42 +0200

    Ensure right owner on authorized_keys2 file.

--- manifests/dcfiles/authkeys.pp
+++ manifests/dcfiles/authkeys.pp
@@ -2,5 +2,11 @@ define dcache::dcfiles::authkeys (
   $file,
   $augeas,
 ) {
+  # We have to ensure $file is owned by $dcache::user, otherwise
+  # ssh_authorized_key will fail updating it.
+  file { "Ensure right owner of '${file}'":
+    owner => $dcache::user,
+  }
+  
   create_resources('ssh_authorized_key', $augeas, { target => $file, user => $dcache::user })
 }
