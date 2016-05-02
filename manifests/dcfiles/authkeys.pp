5c3d548adde44effaf57de4fa68370407bb9bfa5
Xavier Mol <xavier.mol@kit.edu>
2016-05-02 12:23:01 +0200

    Use the augeas resource myself, instead of Puppet's ssh_authorized_keys build-in type, when managing dCache's authorized_keys file for admin.

--- manifests/dcfiles/authkeys.pp
+++ manifests/dcfiles/authkeys.pp
@@ -2,11 +2,19 @@ define dcache::dcfiles::authkeys (
   $file,
   $augeas,
 ) {
-  # We have to ensure $file is owned by $dcache::user, otherwise
-  # ssh_authorized_key will fail updating it.
-  file { "Ensure right owner of '${file}'":
-    owner => $dcache::user,
+  # Unfortunately, we cannot use Puppet's own ssh_authorized_keys type,
+  # because the file is in a location owned by root,
+  # yet Puppet tries to write the file as $dcache::user.
+  Augeas {
+    lens => 'Authorized_Keys.lns',
+    incl => $file,
   }
   
-  create_resources('ssh_authorized_key', $augeas, { target => $file, user => $dcache::user })
+  augeas { 'Manage dCache\'s admin authorized keys':
+    changes => flatten(map($augeas) |$comment, $details| {[
+      "defnode this key[. = '${details[key]}' and type = '${details[type]}' and comment = ${comment}] '${details[key]}'",
+      "set \$this/type '${details[type]}'",
+      "set \$this/comment '${comment}'",
+    ]}),
+  }
 }
