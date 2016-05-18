7027ca9aa724927c999829f0e7c38423852320aa
Xavier Mol <xavier.mol@kit.edu>
2016-05-18 14:51:02 +0200

    Fix as intended.

--- manifests/dcfiles/exports.pp
+++ manifests/dcfiles/exports.pp
@@ -6,22 +6,14 @@ define dcache::dcfiles::exports (
   augeas { "Manage '${file}' with Augeas":
     incl    => $file,
     lens    => 'Exports.lns',
-    changes => flatten(map($augeas) |$dir| {[
-      "defnode dir dir[. = '${dir}']",
-      map($dir) |$client, $options| {[
-        "defnode client \$dir/client[. = '${client}']",
+    changes => flatten(map($augeas) |$dir, $clients| {[
+      "defnode dir dir[. = '${dir}'] '${dir}'",
+      map($clients) |$client, $options| {[
+        "defnode client \$dir/client[. = '${client}'] '${client}'",
         map($options) |$opt| {
           "set \$client/option[. = '${opt}'] '${opt}'"
         }
       ]}
     ]}),
-  } ~>
-  # Since $file is a simple list of arbitrary key value pairs, it is more
-  # readable to have them sorted. Specifically for dcache.conf, where
-  # properties for the same service are prefixed the same.
-  exec { "Sort '${file}'":
-    refreshonly => true,
-    path        => '/bin',
-    command     => "sort -o ${file} ${file}",
   }
 }
