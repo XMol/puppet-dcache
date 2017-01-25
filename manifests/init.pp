e46c80a4e85e7f5ccaaf22918811db1230c66194
Xavier Mol <xavier.mol@kit.edu>
2017-01-25 16:51:24 +0100

    Streamline params content.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,25 +7,25 @@ class dcache (
   
   # A file sourced by dCache on initializiation, so arbitrary environment
   # variables can be set, specifically JAVA and JAVA_HOME.
-  $env_path = $dcache::params::env_path,
+  $env_path = $dcache::params::paths_env,
   $env = {},
   # dCache's central dcache.conf file.
-  $setup_path = $dcache::params::setup_path,
+  $setup_path = $dcache::params::paths_setup,
   $setup = {},
-  $layout_path = $dcache::params::layout_path,
+  $layout_path = $dcache::params::paths_layout,
   $layout_globals = {},
   
-  $poolmanager_path = $dcache::params::poolmanager_path,
-  $authorized_keys_path = $dcache::params::authorized_keys_path,
-  $gplazma_path = $dcache::params::gplazma_path,
-  $authzdb_path = $dcache::params::authzdb_path,
-  $gridmap_path = $dcache::params::gridmap_path,
-  $kpwd_path = $dcache::params::kpwd_path,
-  $vorolemap_path = $dcache::params::vorolemap_path,
-  $tapeinfo_path = $dcache::params::tapeinfo_path,
-  $infoprovider_path = $dcache::params::infoprovider_path,
-  $exports_path = $dcache::params::exports_path,
-  $linkgroupauth_path = $dcache::params::linkgroupauth_path,
+  $poolmanager_path = $dcache::params::paths_pm,
+  $authorized_keys_path = $dcache::params::paths_authkeys,
+  $gplazma_path = $dcache::params::paths_gplazma,
+  $authzdb_path = $dcache::params::paths_authzdb,
+  $gridmap_path = $dcache::params::paths_gridmap,
+  $kpwd_path = $dcache::params::paths_kpwd,
+  $vorolemap_path = $dcache::params::paths_vorolemap,
+  $tapeinfo_path = $dcache::params::paths_tapeinfo,
+  $infoprovider_path = $dcache::params::paths_infoprovider,
+  $exports_path = $dcache::params::paths_exports,
+  $linkgroupauth_path = $dcache::params::paths_lgauth,
 ) inherits dcache::params {
   
   class { 'dcache::install': } ->
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,27 +3,27 @@
 # settings for end users. In other words, set a couple of parameters
 # with dCache's default values, so Puppet knows them.
 class dcache::params {
-  $paths_etc = '/etc/dcache'
-  $paths_admin = "${paths_etc}/admin"
-  $paths_billing = '/var/lib/dcache/billing'
-  $paths_config = '/var/lib/dcache/config'
-  $paths_lib = '/var/lib/dcache'
-  $paths_log = '/var/log/dcache'
-  $paths_gridsecurity = '/etc/grid-security'
-  $env_path = '/etc/dcache.env'
-  $setup_path = "${paths_etc}/dcache.conf"
-  $layout_path = "${paths_etc}/layouts/${::hostname}.conf"
   $user = 'dcache'
   $group = 'dcache'
-  $poolmanager_path = "${paths_config}/poolmanager.conf"
-  $authorized_keys_path = "${paths_admin}/authorized_keys2"
-  $gplazma_path = "${paths_etc}/gplazma.conf"
-  $authzdb_path = "${paths_gridsecurity}/storage-authzdb"
-  $gridmap_path = "${paths_gridsecurity}/grid-mapfile"
-  $kpwd_path = "${paths_etc}/dcache.kpwd"
-  $vorolemap_path = "${paths_gridsecurity}/grid-vorolemap"
-  $tapeinfo_path = "${paths_etc}/tape-info.xml"
-  $infoprovider_path = "${paths_etc}/info-provider.xml"
-  $linkgroupauth_path = "${paths_etc}/LinkGroupAuthorization.conf"
-  $exports_path = '/etc/exports'
+  $paths_etc = '/etc/dcache'
+  $paths_setup = "${paths_etc}/dcache.conf"
+  $paths_layout = "${paths_etc}/layouts/${::hostname}.conf"
+  $paths_admin = "${paths_etc}/admin"
+  $paths_lib = '/var/lib/dcache'
+  $paths_billing = "${paths_lib}/billing"
+  $paths_config = "${paths_lib}/config"
+  $paths_log = '/var/log/dcache'
+  $paths_gridsecurity = '/etc/grid-security'
+  $paths_env = '/etc/dcache.env'
+  $paths_pm = "${paths_config}/poolmanager.conf"
+  $paths_authkeys = "${paths_admin}/authorized_keys2"
+  $paths_gplazma = "${paths_etc}/gplazma.conf"
+  $paths_authzdb = "${paths_gridsecurity}/storage-authzdb"
+  $paths_gridmap = "${paths_gridsecurity}/grid-mapfile"
+  $paths_kpwd = "${paths_etc}/dcache.kpwd"
+  $paths_vorolemap = "${paths_gridsecurity}/grid-vorolemap"
+  $paths_tapeinfo = "${paths_etc}/tape-info.xml"
+  $paths_infoprovider = "${paths_etc}/info-provider.xml"
+  $paths_lgauth = "${paths_etc}/LinkGroupAuthorization.conf"
+  $paths_exports = '/etc/exports'
 }
