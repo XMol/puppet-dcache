411fc81077b7fa1933d394b178e632b2bec86e8a
Xavier Mol <xavier.mol@kit.edu>
2018-05-29 11:02:21 +0200

    Replace exec with file
    the exec resource is too unreliable, file works better to ensure the right owner for rpm created directories

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,18 +2,18 @@ class dcache::install (
   $version = $dcache::version,
 ) {
   require dcache
+
   package { 'dcache' :
     name => "dcache-${version}",
   }
-  
-  # After installation, we need to potentially change the owner of
-  # several directories, where dCache processes need write access.
-  if $dcache::user != 'dcache' {
-    exec { 'Correct wrong dCache user':
-      refreshonly => true,
-      subscribe   => Package['dcache'],
-      path        => '/bin',
-      command     => "chown -R ${dcache::user}:${dcache::group} ${dcache::params::paths_admin} ${dcache::params::paths_lib} ${dcache::params::paths_log}"
-    }
+
+  # Ensure correct owner on directories dCache wants to write into.
+  file { [$dcache::params::paths_admin,
+          $dcache::params::paths_lib,
+          $dcache::params::paths_log]:
+    ensure  => 'directory',
+    owner   => $dcache::user,
+    group   => $dcache::group,
+    require => Package['dcache'],
   }
 }
