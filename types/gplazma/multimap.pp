805871f8f6feff43d4ffb50e818883ebd0942695
Xavier Mol <xavier.mol@kit.edu>
2023-08-28 14:06:22 +0200

    add support for gPlazma's multipath plugin and PoolManager's nested pool groups

--- types/gplazma/multimap.pp
+++ types/gplazma/multimap.pp
@@ -1,5 +1,8 @@
 # @summary Validate the content of the multi-mapfile of gPlazma.
 type Dcache::Gplazma::Multimap = Hash[
   Pattern[/\A(dn|email|entitlement|fqan|gid|group|kerberos|oidc|oidcgrp|op|uid|username):/],
-  Array[Pattern[/\A(dn|email|entitlement|fqan|gid|group|kerberos|oidc|oidcgrp|op|uid|username):/]]
+  Hash[
+    Enum['dn','email','entitlement','fqan','gid','group','kerberos','oidc','oidcgrp','op','uid','username'],
+    Variant[Integer, String]
+  ]
 ]
